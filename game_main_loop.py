13bf9ddb4823c4ff4302dfe112c7a9fce56273dd
tatka <tatkagerzhan@gmail.com>
2025-09-01 22:55:40 +0300

    Initial commit

---
+++ game_main_loop.py
@@ -0,0 +1,12 @@
+import os
+import pygame as pg
+import pyautogui
+import random
+from sys import exit
+
+
+modes = {0: 'Game', 
+         1: 'New Level Transition: stage 1',
+         2: 'New Level Transition: stage 2',
+         3: 'In a store',
+         4: 'Game Over'}
